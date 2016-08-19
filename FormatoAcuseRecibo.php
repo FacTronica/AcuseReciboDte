2b18dbb1875a204ece41a2680608a7f554dbea11
FacTrónica <webmaster@factronica.cl>
2016-08-19 00:03:18 -0300

    Update FormatoAcuseRecibo.php

--- FormatoAcuseRecibo.php
+++ FormatoAcuseRecibo.php
@@ -1,1 +1,39 @@
-afsfa
+<?php
+############################################################## 
+#######	CARATULA
+##############################################################
+$Caratula["RutResponde"]="15341196-4";
+$Caratula["RutRecibe"]="88888888-8";
+$Caratula["IdRespuesta"]="1";
+$Caratula["NroDetalles"]="1";
+$Caratula["NmbContacto"]="JUAN PRUEBAS";
+$Caratula["FonoContacto"]="55555";
+$Caratula["MailContacto"]="contacto@pruebas.cl";
+
+##############################################################   
+#######	 RecepcionEnvio
+##############################################################
+$RecepcionEnvio["NmbEnvio"]="ENVIO_DTE_464744.xml";
+$RecepcionEnvio["FchRecep"]="2016-01-13T09:07:28";
+$RecepcionEnvio["CodEnvio"]="1";
+$RecepcionEnvio["EnvioDTEID"]="464744";
+$RecepcionEnvio["Digest"]="";
+$RecepcionEnvio["RutEmisor"]="88888888-8";
+$RecepcionEnvio["RutReceptor"]="15341196-4";
+$RecepcionEnvio["EstadoRecepEnv"]="0";
+$RecepcionEnvio["RecepEnvGlosa"]="DTE recibido conforme";
+$RecepcionEnvio["NroDTE"]="2";
+
+##############################################################
+#######	 RecepcionDTE
+##############################################################
+# DOCUMENTO 
+$RecepcionDTE["TipoDTE"]["1"]="33";
+$RecepcionDTE["Folio"]["1"]="52659";
+$RecepcionDTE["FchEmis"]["1"]="2016-01-13";
+$RecepcionDTE["RUTEmisor"]["1"]="88888888-8";
+$RecepcionDTE["RUTRecep"]["1"]="15341196-4";
+$RecepcionDTE["MntTotal"]["1"]="19242";
+$RecepcionDTE["EstadoRecepDTE"]["1"]="0"; 
+$RecepcionDTE["RecepDTEGlosa"]["1"]="RECIBIDO OK";
+?>
